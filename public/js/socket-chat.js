a3adb60d1213d8c2cf269c3c355af20ca2ea1bec
Minerva <mduranm@ruv.org.mx>
2021-01-13 19:10:58 -0600

    Finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -2,7 +2,7 @@ var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-if (!params.has("nombre") || !params.has("sala")) {
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
@@ -10,13 +10,18 @@ if (!params.has("nombre") || !params.has("sala")) {
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
-    })
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
+    });
+
 });
 
 // escuchar
@@ -24,36 +29,34 @@ socket.on('disconnect', function() {
 
     console.log('Perdimos conexión con el servidor');
 
-
 });
 
 
 // Enviar información
-/*socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});
-*/
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom()
 });
 
-//Cuando un usuario entre y sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log('Servidor:', personas);
-
+    renderizarUsuarios(personas);
+    //console.log(personas);
 });
 
-//Mensajes Privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('Servidor:', mensaje);
+    console.log('Mensaje Privado:', mensaje);
 
 });
